b8a144fa9bbdd85683166f81c3ce78f932478d3e
Natthawut0 <jacktherirper@gmail.com>
2021-08-04 19:28:58 +0700

    master

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,55 +1,65 @@
 <template>
-  <div>
-  <h1> Get All User</h1>
-  <div v-if="users.length">
-    <div><h4>จำนวนผู้ใช้งาน : {{users.length}}</h4></div>
-    <p><button v-on:click="navigateTo('/user/create')">สร้างผู้ใช้</button></p>
-    <div v-for="users in users" v-bind:key="users.id">
-      <div>id : {{users.id }} </div>
-      <div>ชื่อนามสกุล : {{users.name }} - {{users.lastname}}</div>
-      <div>email : {{users.email}}</div>
-      <div>password : {{users.password}}</div>
-      <button v-on:click="navigateTo('/user/'+users.id)">ดูข้อมูลผู้ใช้</button>
-      <button v-on:click="navigateTo('/user/edit/'+users.id)">แก้ไขข้อมูลผู้ใช้</button>
-      <button v-on:click="deleteUser(user)">ลบข้อมูลผู้ใช้</button>
-      <hr>
+ <div>
+    <h1> Get All Users </h1>
+    <h4>จำนวนผู้ใช้งาน {{ users.length }}</h4>
+
+   <p> <button v-on:click="navigateTo('/user/create/')">สร้างผู้ใช้</button></p>
+
+    <div v-for="user in users" v-bind:key="user.id">
+
+        <p>id : {{ user.id }}</p>
+        <p>ชื่อ-นามสกุล : {{ user.name }} - {{ user.lastname }}</p>
+        <p>email : {{ user.email }}</p>
+        <p>password : {{ user.password}}</p>
+
+        <p><button v-on:click="navigateTo('/user/'+ user.id)">ดูข้อมูลผู้ใช้</button>
+        <button v-on:click="navigateTo('/user/edit/'+ user.id)">แก้ไขข้อมูล</button>
+        <button v-on:click="deleteUser(user)">ลบข้อมูล</button>
+        </p>
+
+        <hr>
     </div>
-  </div>
 </div>
 </template>
 <script>
-import UserService from '@/services/UserService'
 
-export default {
-  data(){
-    return{
-      users:[],
-    };
-  },
-  async created(){
-    try{
-      this.users = (await UserService.index()).data
-    }catch(error){
-      console.log(error);
-    }
-  },
-  methods:{
-    navigateTo(route){
-      this.$router.push(route)
-    },
-    async deleteUser(user){
-      let result = confirm("want to delete")
-      if (result){
-        try{
-          await UserService.delete(user)
-        }catch(error){
-          console.log(error)
+import UsersService from '@/services/UserService'
+
+    export default {
+        data () {
+            return {
+                users : []
+            }
+        },
+       async created (){
+           this.users = (await UsersService.index()).data
+           console.log(results)
+        },
+        methods: {
+            navigateTo (route){
+                this.$router.push(route)
+
+        },
+        async deleteUser(user) {
+            let result = confirm("What To Delete?")
+               if(result) {
+                   try {
+                    await UsersService.delete(user)
+                    this.refreshData()
+            } catch (error){
+            console.log(error)
+          }
         }
-      }
+         },
+         async refreshData() {
+             this.users = (await UsersService.index()).data
+         }
     }
-  },
-};
+}
+
+
 </script>
 <style scoped>
+
 
 </style>
